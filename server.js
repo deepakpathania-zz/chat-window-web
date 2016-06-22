95c92a9222407b2425303fbb22dcaff25755742f
deepakpathania <deepakpathania789@gmail.com>
2016-06-22 15:28:14 +0530

    localstorage added for data persistence

--- server.js
+++ server.js
@@ -14,17 +14,21 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err, db){
 		col.find().limit(100).sort({_id : 1}).toArray(function(err, res) {
 			if(err) throw err;
 			socket.emit('output', res);
+
 		});
 		//wait for input
 		socket.on('input', function(data) {
 			var name = data.name;
 			var message = data.message;
+			var time = new Date();
+			data.created = time;
+			console.log(time);
 			whiteSpacePattern = /^\s*$/;
 			if(whiteSpacePattern.test(name) || whiteSpacePattern.test(message)) {
 				sendStatus("Name and message required");
 			}
 			else {
-			col.insert({name : name, message : message} , function() {
+			col.insert({name : name, message : message, created : time} , function() {
 				console.log("inserted");
 				client.emit('output',[data]);
 				sendStatus({
